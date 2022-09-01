7a47440d513097f71961b0be2aa9deace348be57
Pi-Guy53 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-01 12:00:19 -0400

    plane, player, base input system

---
+++ Assets/_scripts/RollerBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerBall : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
